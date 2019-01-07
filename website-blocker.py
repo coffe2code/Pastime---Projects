6cff6e514fc38f713b2914edbf9bc506d76f9974
P SAI PRASANTH <psai.prasanth.min16@itbhu.ac.in>
2019-01-07 23:52:23 +0530

    Added website blocker

---
+++ website-blocker.py
@@ -0,0 +1,49 @@
+import datetime
+
+blocked_websites = ['www.facebook.com','www.instagram.com','www.netflix.com','www.quora.com','www.youtube.com']
+
+file_path = "/etc/hosts"
+
+local_host = "127.0.0.1"
+
+def block():
+
+	for website in blocked_websites:
+
+		with open(file_path,'a') as f:
+			clause = local_host+"	"+website+"\n"
+			f.write(clause)
+
+def unblock():
+
+	with open(file_path,'r+') as f:
+		lines = f.readlines()
+		lines_to_delete = [local_host+"	"+site+"\n" for site in blocked_websites]
+		f.seek(0)
+		for line in lines:
+			if line in lines_to_delete:
+				pass
+			else:
+				f.write(line)
+		f.truncate()
+
+def block_checker():
+
+	with open(file_path,'r') as f:
+		lines = f.readlines()
+		clause = local_host+"	"+blocked_websites[0]+"\n"
+		if clause in lines:
+			return True
+		else:
+			return False
+
+
+if __name__ == "__main__":
+
+	current_hour = datetime.datetime.now().hour
+	if current_hour >=7 and current_hour<=21:
+
+		if not block_checker():
+			block()
+	else:
+		unblock()
